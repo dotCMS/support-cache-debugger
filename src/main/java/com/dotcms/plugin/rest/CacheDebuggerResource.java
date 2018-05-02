337bb4a7cfe3078c991a5fbcfdac1e0e8ac3a95f
joseorsini <jose@dotcms.com>
2018-05-02 14:45:24 -0500

    first draft for v4.2.2

--- src/main/java/com/dotcms/plugin/rest/CacheDebuggerResource.java
+++ src/main/java/com/dotcms/plugin/rest/CacheDebuggerResource.java
@@ -1,92 +1,48 @@
-package com.dotcms.plugin.rest;
-
-import java.net.URISyntaxException;
-
-import javax.servlet.http.HttpServletRequest;
+package com.dotcms.rest;
 
 import com.dotcms.repackage.javax.ws.rs.GET;
-import com.dotcms.repackage.javax.ws.rs.POST;
-import com.dotcms.repackage.javax.ws.rs.PUT;
 import com.dotcms.repackage.javax.ws.rs.Path;
 import com.dotcms.repackage.javax.ws.rs.PathParam;
+import com.dotcms.repackage.javax.ws.rs.Produces;
 import com.dotcms.repackage.javax.ws.rs.core.CacheControl;
 import com.dotcms.repackage.javax.ws.rs.core.Context;
+import com.dotcms.repackage.javax.ws.rs.core.MediaType;
 import com.dotcms.repackage.javax.ws.rs.core.Response;
 import com.dotcms.repackage.javax.ws.rs.core.Response.ResponseBuilder;
-import com.dotcms.rest.InitDataObject;
-import com.dotcms.rest.WebResource;
+import com.dotcms.repackage.org.glassfish.jersey.server.JSONP;
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.DotStateException;
 import com.dotmarketing.exception.DotDataException;
 import com.dotmarketing.exception.DotSecurityException;
 import com.liferay.portal.model.User;
+import javax.servlet.http.HttpServletRequest;
 
-@Path("/example")
+@Path("/cacheDebugger")
 public class CacheDebuggerResource  {
 
     private final WebResource webResource = new WebResource();
 
-	@PUT
-	public Response doPut(@Context HttpServletRequest request, @PathParam("params") String params) throws URISyntaxException {
-
-		InitDataObject auth = webResource.init(true, request, false);
-		CacheControl cc = new CacheControl();
-		cc.setNoCache(true);
-		User user = auth.getUser();
-		String username = (user != null) ? user.getFullName() : " unknown ";
-		ResponseBuilder builder = Response.ok("{\"result\":\"" + username + " PUT!\"}", "application/json");
-		return builder.cacheControl(cc).build();
-	}
-
-	@POST
-	public Response doPost(@Context HttpServletRequest request, @PathParam("params") String params) throws URISyntaxException {
-		InitDataObject auth = webResource.init(true, request, false);
-		User user = auth.getUser();
-		String username = (user != null) ? user.getFullName() : " unknown ";
-		CacheControl cc = new CacheControl();
-		cc.setNoCache(true);
-		ResponseBuilder builder = Response.ok("{\"result\":\"" + username + " POST!\"}", "application/json");
-		return builder.cacheControl(cc).build();
-	}
-
-	/**
-	 * This is an authenticated rest service.
-	 * 
-	 * @param request
-	 * @param cache
-	 * @return
-	 * @throws DotStateException
-	 * @throws DotDataException
-	 * @throws DotSecurityException
-	 */
-	@GET
-    @NoCache
+    /**
+     * This resource is meant to look for a cache Key on different cache regions
+     * if a match is found, it will return its information
+     * along with the place where it should be located
+     *
+     * @param request
+     * @param cacheKey
+     * @return
+     * @throws DotStateException
+     * @throws DotDataException
+     * @throws DotSecurityException
+     */
+    @GET
     @JSONP
-	@Path("/cacheKey/{cacheKey}")
+    @Path("/cacheKey/{cacheKey}")
     @Produces({MediaType.APPLICATION_JSON, "application/javascript"})
-	public Response loadJson(@Context HttpServletRequest request, @PathParam("params") String params) throws DotStateException,
-			DotDataException, DotSecurityException {
-		// force authentication
-		InitDataObject auth = webResource.init(true, request, true);
-		User user = auth.getUser();
-		String username = (user != null) ? user.getFullName() : " unknown ";
-		CacheControl cc = new CacheControl();
-		cc.setNoCache(true);
-		ResponseBuilder builder = Response.ok("{\"result\":\"/test/" + username + " GET!\"}", "application/json");
-		return builder.cacheControl(cc).build();
-
-	}
-
-	@GET
-	public Response loadRoot(@Context HttpServletRequest request, @PathParam("params") String params) throws DotStateException,
-			DotDataException, DotSecurityException {
-		InitDataObject auth = webResource.init(true, request, false);
-		User user = auth.getUser();
-		String username = (user != null) ? user.getFullName() : " unknown ";
-		CacheControl cc = new CacheControl();
-		cc.setNoCache(true);
-		ResponseBuilder builder = Response.ok("{\"result\":\"" + username + " GET!\"}", "application/json");
-		return builder.cacheControl(cc).build();
-
-	}
+    public Response loadJson(@Context HttpServletRequest request, @PathParam("cacheKey") String cacheKey) throws DotStateException {
+        User user = APILocator.systemUser();
+        String username = (user != null) ? user.getFullName() : " unknown ";
+        ResponseBuilder builder = Response.ok("{\"result\":\"/test/" + username + " GET!\"}", "application/json");
+        return builder.build();
+    }
 
 }
